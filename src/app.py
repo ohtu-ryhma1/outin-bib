cc788d5dcd91cfb0f486aadca263e23554a31dde
petrovaana <petrova.anastasiahusu@gmail.com>
2025-11-13 14:53:46 +0200

    Modified app.py

--- src/app.py
+++ src/app.py
@@ -1,30 +1,29 @@
 from flask import redirect, render_template, request, jsonify, flash
 from db_helper import reset_db
-from repositories.todo_repository import get_todos, create_todo, set_done
 from config import app, test_env
-from util import validate_todo
+from repositories.reference_repository import get_references, create_reference
 
 @app.route("/")
 def index():
-    todos = get_todos()
-    unfinished = len([todo for todo in todos if not todo.done])
-    return render_template("index.html", todos=todos, unfinished=unfinished)
+    references = get_references()
+    return render_template("index.html", references=references)
 
-@app.route("/new_todo")
+@app.route("/new_ref")
 def new():
-    return render_template("new_todo.html")
+    return render_template("new_ref.html")
 
-@app.route("/create_todo", methods=["POST"])
-def todo_creation():
-    content = request.form.get("content")
+@app.route("/create_ref", methods=["POST"])
+def ref_creation():
+    title = request.form.get("title")
+    author = request.form.get("author")
+    year = request.form.get("year")
 
     try:
-        validate_todo(content)
-        create_todo(content)
+        create_reference(title, author, year)
         return redirect("/")
     except Exception as error:
         flash(str(error))
-        return  redirect("/new_todo")
+        return  redirect("/new_ref")
 
 @app.route("/toggle_todo/<todo_id>", methods=["POST"])
 def toggle_todo(todo_id):
