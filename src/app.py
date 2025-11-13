2a5cfd34cd4a2fca4c93afac380b8548b59c9a89
vekkul1 <veeti.hallaaho@gmail.com>
2025-11-13 19:39:20 +0200

    fix: fixed typos and overlap

--- src/app.py
+++ src/app.py
@@ -8,31 +8,21 @@ def index():
     references = get_references()
     return render_template("index.html", references=references)
 
-@app.route("/new_reference")
-def new():
-    return render_template("new_reference.html")
-
-@app.route("/create_reference", methods=["POST"])
-def ref_creation():
-    title = request.form.get("title")
-    author = request.form.get("author")
-    year = request.form.get("year")
-
-    try:
-        create_reference(title, author, year)
-        return redirect("/")
-    except Exception as error:
-        flash(str(error))
-        return  redirect("/new_reference")
-
-@app.route("/create_refrence", methods=["GET", "POST"])
-def refrence_creation():
-
+@app.route("/new_reference", methods=["GET", "POST"])
+def new_ref():
     if request.method == "GET":
-        return render_template("new_refrence.html")
+        return render_template("new_reference.html")
 
     if request.method == "POST":
-        return jsonify(request.form)
+        title = request.form.get("title")
+        author = request.form.get("author")
+        year = request.form.get("year")
+        try:
+            create_reference(title, author, year)
+            return redirect("/")
+        except Exception as error:
+            flash(str(error))
+            return  redirect("/new_reference")
 
 @app.route("/toggle_todo/<todo_id>", methods=["POST"])
 def toggle_todo(todo_id):
