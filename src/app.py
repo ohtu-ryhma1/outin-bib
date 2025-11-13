78afd3c5a851ce77ec8b4e39435e2e64ceec18ad
petrovaana <petrova.anastasiahusu@gmail.com>
2025-11-13 15:09:22 +0200

    fix : fixed a bug

--- src/app.py
+++ src/app.py
@@ -8,11 +8,11 @@ def index():
     references = get_references()
     return render_template("index.html", references=references)
 
-@app.route("/new_ref")
+@app.route("/new_reference")
 def new():
-    return render_template("new_ref.html")
+    return render_template("new_reference.html")
 
-@app.route("/create_ref", methods=["POST"])
+@app.route("/create_reference", methods=["POST"])
 def ref_creation():
     title = request.form.get("title")
     author = request.form.get("author")
@@ -23,7 +23,7 @@ def ref_creation():
         return redirect("/")
     except Exception as error:
         flash(str(error))
-        return  redirect("/new_ref")
+        return  redirect("/new_reference")
 
 @app.route("/toggle_todo/<todo_id>", methods=["POST"])
 def toggle_todo(todo_id):
