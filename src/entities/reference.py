2e0644b5a9fc26c5f62ab936a79ca082b16092ae
MarkusSKoo <markus.kauhanen@helsinki.fi>
2025-11-14 09:24:43 +0200

    feat(entities): add Reference class

--- src/entities/reference.py
+++ src/entities/reference.py
@@ -1,2 +1,9 @@
 class Reference:
-    pass
+    def __init__(self, id, title, author, year):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __repr__(self):
+        return f"<reference_id={self.id} title={self.title}>"
