f9fe8aac23f4bc5c6e36eaeea06d3b05c35d6e8c
MarkusSKoo <markus.kauhanen@helsinki.fi>
2025-11-14 13:55:16 +0200

    fix: rename id field

--- src/entities/reference.py
+++ src/entities/reference.py
@@ -1,9 +1,9 @@
 class Reference:
-    def __init__(self, id, title, author, year):
-        self.id = id
+    def __init__(self, entry_id, title, author, year):
+        self.entry_id = entry_id
         self.title = title
         self.author = author
         self.year = year
 
     def __repr__(self):
-        return f"<reference_id={self.id} title={self.title}>"
+        return f"<reference_id={self.entry_id} title={self.title}>"
