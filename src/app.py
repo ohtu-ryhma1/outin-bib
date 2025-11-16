ba3edc6cb6eb615c5bd7b4db0f0f65c5796233da
hatonlee <hatonen.leevi@gmail.com>
2025-11-16 17:04:17 +0200

    feat: updated to use reference service

--- src/app.py
+++ src/app.py
@@ -1,33 +1,30 @@
 from flask import redirect, render_template, request, jsonify, flash
 from db_helper import reset_db
 from config import app, test_env
-from repositories.reference_repository import get_references, create_reference
+
+from services.reference_service import reference_service
 
 @app.route("/")
 def index():
-    references = get_references()
+    references = reference_service.get_references()
     return render_template("index.html", references=references)
 
 @app.route("/new_reference", methods=["GET", "POST"])
-def new_ref():
+def new_reference():
     if request.method == "GET":
         return render_template("new_reference.html")
 
     if request.method == "POST":
-        fields = {key: value for key, value in request.form.items() if key != "Type"}
-        type_name = request.form.get("Type")
+        reference_type = request.form.get("type")
+        reference_name = request.form.get("name")
+        fields = {key: value for key, value in request.form.items() if key != "type"}
 
         try:
-            create_reference(type_name, fields)
+            reference_service.create_reference(reference_type, reference_name, fields)
             return redirect("/")
         except Exception as error:
             flash(str(error))
             return redirect("/new_reference")
-
-@app.route("/toggle_todo/<todo_id>", methods=["POST"])
-def toggle_todo(todo_id):
-    set_done(todo_id)
-    return redirect("/")
 
 if test_env:
     @app.route("/reset_db")
