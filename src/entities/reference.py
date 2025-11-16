a2acd5769e9afd09c1d2d35f73a1f422e2d781ed
hatonlee <hatonen.leevi@gmail.com>
2025-11-16 17:01:35 +0200

    feat: added properties

--- src/entities/reference.py
+++ src/entities/reference.py
@@ -1,23 +1,23 @@
 class Reference:
     def __init__(self, reference_type, name, fields=None):
-        self.type = reference_type
-        self.name = name
-        self.fields = fields or []
+        self._type = reference_type
+        self._name = name
+        self._fields = fields or []
 
     def __repr__(self):
         return f"Reference(type={self.type}, name={self.name}, fields={self.fields}"
 
     @property
     def reference_type(self):
-        return self.type
+        return self._type
 
     @property
     def name(self):
-        return self.name
+        return self._name
 
     @property
     def fields(self):
-        return self.fields
+        return self._fields
 
     def get_field(self, field_name):
         fields = set(filter(lambda f: f.name == field_name, self.fields))
