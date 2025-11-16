795665d770bb8db64a2f0c06af749d5011edf3d5
hatonlee <hatonen.leevi@gmail.com>
2025-11-16 17:05:01 +0200

    feat: added basic reference and field types

---
+++ src/init.sql
@@ -0,0 +1,17 @@
+INSERT INTO reference_types (name)
+     VALUES ('book');
+
+INSERT INTO reference_types (name)
+     VALUES ('article');
+
+INSERT INTO reference_types (name)
+     VALUES ('website');
+
+INSERT INTO field_types (name)
+     VALUES ('title');
+
+INSERT INTO field_types (name)
+     VALUES ('author');
+
+INSERT INTO field_types (name)
+     VALUES ('date');
