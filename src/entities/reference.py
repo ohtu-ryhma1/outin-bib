66f1cc4e99047e9be7164331dd344fa077106d64
hatonlee <hatonen.leevi@gmail.com>
2025-11-16 14:00:20 +0200

    feat: updated properties and methods

--- src/entities/reference.py
+++ src/entities/reference.py
@@ -1,17 +1,24 @@
 class Reference:
-    def __init__(self, entry_id, type_name, fields=None):
-        self.entry_id = entry_id
-        self.type_name = type_name
+    def __init__(self, reference_type, name, fields=None):
+        self.type = reference_type
+        self.name = name
         self.fields = fields or []
 
-    def get(self, field_name):
-        for f in self.fields:
-            if f.name == field_name:
-                return f.value
-        return None
-
     def __repr__(self):
-        return f"<Reference id={self.entry_id} type={self.type_name} fields={len(self.fields)}>"
+        return f"Reference(type={self.type}, name={self.name}, fields={self.fields}"
 
-    def as_dict(self):
-        return {f.name: f.value for f in self.fields}
+    @property
+    def reference_type(self):
+        return self.type
+
+    @property
+    def name(self):
+        return self.name
+
+    @property
+    def fields(self):
+        return self.fields
+
+    def get_field(self, field_name):
+        fields = set(filter(lambda f: f.name == field_name, self.fields))
+        return fields[0] if fields else None
