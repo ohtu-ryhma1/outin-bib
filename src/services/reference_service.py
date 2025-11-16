76ff456f0dcfcdd536857607b48028f8ee08e5eb
hatonlee <hatonen.leevi@gmail.com>
2025-11-16 17:02:53 +0200

    feat: implemented service for references

--- src/services/reference_service.py
+++ src/services/reference_service.py
@@ -1,4 +1,20 @@
-from repositories.reference_repository import get_references, create_reference
+from repositories.reference_repository import reference_repository
 from entities.reference import Reference
 from entities.field import Field
 
+class ReferenceService:
+    def __init__(self, reference_repository=reference_repository):
+        self._reference_repository = reference_repository
+
+    def get_references(self):
+        return self._reference_repository.get_references()
+
+    def create_reference(self, reference_type, reference_name, fields):
+        # create field objects
+        fields = [Field(name, value) for name, value in fields.items()]
+
+        # create reference object
+        reference = Reference(reference_type, reference_name, fields)
+        return self._reference_repository.create_reference(reference)
+
+reference_service = ReferenceService()
