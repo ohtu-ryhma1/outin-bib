7d0fc52ad413f26bd8b61c2e2ad792a1d6f06fa4
hatonlee <hatonen.leevi@gmail.com>
2025-11-16 17:07:18 +0200

    feat: added test types and fixed indentation

--- src/db_helper.py
+++ src/db_helper.py
@@ -3,47 +3,63 @@ from sqlalchemy import text
 import os
 
 def reset_db():
-  print(f"Clearing contents from table todos")
-  sql = text(f"DELETE FROM todos")
-  db.session.execute(sql)
-  db.session.commit()
-
-def tables():
-  """Returns all table names from the database except those ending with _id_seq"""
-  sql = text(
-    "SELECT table_name "
-    "FROM information_schema.tables "
-    "WHERE table_schema = 'public' "
-    "AND table_name NOT LIKE '%_id_seq'"
-  )
-
-  result = db.session.execute(sql)
-  return [row[0] for row in result.fetchall()]
-
-def setup_db():
-  """
-    Creating the database
-    If database tables already exist, those are dropped before the creation
-  """
-  tables_in_db = tables()
-  if len(tables_in_db) > 0:
-    print(f"Tables exist, dropping: {', '.join(tables_in_db)}")
-    for table in tables_in_db:
-      sql = text(f"DROP TABLE {table}")
-      db.session.execute(sql)
+    print("Clearing contents from table references")
+    sql = text("DELETE FROM references")
+    db.session.execute(sql)
     db.session.commit()
 
-  print("Creating database")
 
-  # Read schema from schema.sql file
-  schema_path = os.path.join(os.path.dirname(__file__), 'schema.sql')
-  with open(schema_path, 'r') as f:
-    schema_sql = f.read().strip()
+def tables():
+    """Returns all table names from the database except those ending with _id_seq"""
+    sql = text(
+        "SELECT table_name "
+        "FROM information_schema.tables "
+        "WHERE table_schema = 'public' "
+        "AND table_name NOT LIKE '%_id_seq'"
+    )
 
-  sql = text(schema_sql)
-  db.session.execute(sql)
-  db.session.commit()
+    result = db.session.execute(sql)
+    return [row[0] for row in result.fetchall()]
+
+
+def setup_db():
+    """
+    Creating the database
+    If database tables already exist, those are dropped before the creation
+    """
+
+    tables_in_db = tables()
+    if len(tables_in_db) > 0:
+        print(f"Tables exist, dropping: {', '.join(tables_in_db)}")
+        for table in tables_in_db:
+            sql = text(f"DROP TABLE {table}")
+            db.session.execute(sql)
+    db.session.commit()
+
+    print("Creating database")
+
+    # get schema file
+    schema_path = os.path.join(os.path.dirname(__file__), 'schema.sql')
+    with open(schema_path, 'r') as f:
+        schema_sql = f.read().strip()
+
+    sql = text(schema_sql)
+    db.session.execute(sql)
+    db.session.commit()
+
+
+def add_test_types():
+    print("Adding test types")
+    # get init file
+    schema_path = os.path.join(os.path.dirname(__file__), 'init.sql')
+    with open(schema_path, 'r') as f:
+        schema_sql = f.read().strip()
+
+    sql = text(schema_sql)
+    db.session.execute(sql)
+    db.session.commit()
 
 if __name__ == "__main__":
     with app.app_context():
-      setup_db()
+        # setup_db()
+        add_test_types()
