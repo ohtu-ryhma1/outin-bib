65a60a9c82ea9614bb8443f75af655a207f3596a
hatonlee <hatonen.leevi@gmail.com>
2025-11-19 19:31:43 +0200

    feat: initial support for ORM Declarative Mappings

--- src/index.py
+++ src/index.py
@@ -1,4 +1,11 @@
 from app import app
+from config import db
 
 if __name__ == "__main__":
+
+    # initialize db tables
+    with app.app_context():
+        db.create_all()
+
+    # start the app
     app.run(port=5001, host="0.0.0.0", debug=True)
