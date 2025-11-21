405c3ee178f3a3085af34608cbb13db4a30bb7b3
hatonlee <hatonen.leevi@gmail.com>
2025-11-21 09:36:48 +0200

    feat: converted methods for ORM service

--- src/app.py
+++ src/app.py
@@ -1,36 +1,32 @@
 from flask import redirect, render_template, request, jsonify, flash
-from config import app
+
+from config import app, db
 from config import test_env
 
-from services.reference_service import reference_service
+from services.reference_service import reference_service as ref_service
 
-from db_helper import reset_db
 
-@app.route("/")
+@app.get("/")
 def index():
-    references = reference_service.get_references()
+    references = ref_service.get_all()
     return render_template("index.html", references=references)
 
-@app.route("/new_reference", methods=["GET", "POST"])
-def new_reference():
-    if request.method == "GET":
-        return render_template("new_reference.html")
 
-    if request.method == "POST":
-        reference_type = request.form.get("type")
-        reference_name = request.form.get("name")
-        fields = {key: value for key, value in request.form.items() if key != "type"}
+@app.get("/new_reference")
+def show_new_reference():
+    return render_template("new_reference.html")
 
-        try:
-            reference_service.create_reference(reference_type, reference_name, fields)
-            return redirect("/")
-        except Exception as error:
-            flash(str(error))
-            return redirect("/new_reference")
 
-# testing routes
-if test_env:
-    @app.route("/reset_db")
-    def reset_database():
-        reset_db()
-        return redirect("/")
+@app.post("/new_reference")
+def create_new_reference():
+    ref_type = request.form.get("type")
+    ref_name = request.form.get("name")
+    fields = {key: value for key, value in request.form.items() if key not in ("type", "name")}
+    ref_data = {
+        "type": ref_type,
+        "name": ref_name,
+        "fields": fields
+    }
+
+    ref_service.create(ref_data)
+    return redirect("/")
