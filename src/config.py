30461b6e91b890710160557df0c9d1dda2652349
hatonlee <hatonen.leevi@gmail.com>
2025-11-21 09:37:32 +0200

    feat: added support for in-memory database

--- src/config.py
+++ src/config.py
@@ -17,8 +17,9 @@ print(f"Test environment: {test_env}")
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 
-# use in-memory database if test_env is true
-DB_URL = getenv("DEV_DATABASE_URL") if test_env else getenv("DATABASE_URL")
+# use in-memory database if test_db is true
+test_db = getenv("TEST_DB") == "true"
+DB_URL = getenv("TEST_DB_URL") if test_db else getenv("PRODUCTION_DB_URL")
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
 
 # initialize database instance and connect it to the app instance
