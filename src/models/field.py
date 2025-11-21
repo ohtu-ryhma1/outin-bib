3d056ac86323588c6ffa6842a080e8f0ae632de6
hatonlee <hatonen.leevi@gmail.com>
2025-11-21 09:34:38 +0200

    style: added whitespace

--- src/models/field.py
+++ src/models/field.py
@@ -4,11 +4,13 @@ from sqlalchemy.orm import relationship
 
 from config import db
 
+
 class Field(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     type: Mapped[str]
     value: Mapped[str]
 
+    reference_id: Mapped[int] = mapped_column(db.ForeignKey("reference.id"))
     reference: Mapped["Reference"] = relationship(back_populates="fields")
 
     def __repr__(self) -> str:
--- src/models/reference.py
+++ src/models/reference.py
@@ -2,7 +2,10 @@ from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import relationship
 
+from typing import List
+
 from config import db
+
 
 class Reference(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
