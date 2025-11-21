83e85d43ffe740f512877831cfa43f5dda3cc654
hatonlee <hatonen.leevi@gmail.com>
2025-11-21 12:55:11 +0200

    feat: removed unused import

--- src/services/reference_service.py
+++ src/services/reference_service.py
@@ -1,6 +1,5 @@
 from repositories.reference_repository import reference_repository
-from entities.reference import Reference
-from entities.field import Field
+
 
 class ReferenceService:
     def __init__(self, repo):
@@ -11,6 +10,7 @@ class ReferenceService:
 
     def create(self, ref_data: dict) -> Reference:
         return self._repo.create(ref_data)
+
 
 # default service
 reference_service = ReferenceService(reference_repository)
