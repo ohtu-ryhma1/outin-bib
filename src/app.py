890d72a4072a261d093e9a8f52b656ab32ca8fc6
hatonlee <hatonen.leevi@gmail.com>
2025-11-24 00:37:58 +0200

    feat: added POST route for editing references

--- src/app.py
+++ src/app.py
@@ -28,7 +28,7 @@ def show_new_reference():
 
 
 @app.post("/new_reference")
-def create_new_reference():
+def new_reference():
     ref_type = request.form.get("type")
     ref_name = request.form.get("name")
     fields = {
@@ -53,8 +53,6 @@ def create_new_reference():
 
 @app.get("/edit_reference")
 def show_edit_reference():
-
-    # get reference
     ref_id = request.args.get("id")
     ref = ref_service.get(ref_id=ref_id)
 
@@ -70,3 +68,30 @@ def show_edit_reference():
         required=required,
         optional=optional,
     )
+
+
+@app.post("/edit_reference")
+def edit_reference():
+    ref_id = request.form.get("id")
+    ref_type = request.form.get("type")
+    ref_name = request.form.get("name")
+    fields = {
+        key: value
+        for key, value in request.form.items()
+        if key not in ("id", "type", "name")
+    }
+
+    ref_data = {
+        "type": ref_type,
+        "name": ref_name,
+        "fields": fields,
+    }
+
+    try:
+        ref_service.update(ref_id, ref_data)
+        flash("Reference updated succesfully")
+        return redirect("/")
+
+    except ValueError as error:
+        flash(str(error), "error")
+        return redirect(url_for("edit_reference", ref_id=ref_id, type=ref_type))
