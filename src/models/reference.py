eb957f9f6adc95c941e55feb4c50f2494598b038
hatonlee <hatonen.leevi@gmail.com>
2025-11-23 22:29:24 +0200

    feat: implemented cascade to fields

--- src/models/reference.py
+++ src/models/reference.py
@@ -10,7 +10,9 @@ class Reference(db.Model):
     type: Mapped[str]
     name: Mapped[str] = mapped_column(unique=True)
 
-    fields: Mapped[List["Field"]] = relationship(back_populates="reference")
+    fields: Mapped[List["Field"]] = relationship(
+        back_populates="reference", cascade="all, delete-orphan"
+    )
 
     def __repr__(self) -> str:
         return f"Reference(id={self.id!r}, type={self.type!r}, name={self.name!r})"
