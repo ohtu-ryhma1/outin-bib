aa593fe9850ec66ebcd870d3affda1b12693e314
hatonlee <hatonen.leevi@gmail.com>
2025-11-23 22:34:27 +0200

    feat: implemented get and update methods

--- src/services/reference_service.py
+++ src/services/reference_service.py
@@ -14,9 +14,16 @@ class ReferenceService:
     def get_all(self) -> Iterable[ScalarResult]:
         return self._repo.get_all()
 
+    def get(self, ref_id: id = None) -> Reference:
+        return self._repo.get(ref_id)
+
     def create(self, ref_data: dict) -> Reference:
         validate_reference(ref_data)
         return self._repo.create(ref_data)
+
+    def update(self, ref_id: int, ref_data: dict) -> Reference:
+        validate_reference(ref_data)
+        return self._repo.update(ref_id, ref_data)
 
 
 # default service
