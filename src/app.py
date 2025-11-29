b0ac7bc0f8fb24a46ee114161bb521f8b92c8a52
hatonlee <hatonen.leevi@gmail.com>
2025-11-29 15:49:26 +0200

    feat: added methods for metadata, updated routes

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ from src.services.reference_types import get_reference_fields, get_reference_typ
 
 @app.get("/")
 def index():
-    refs = ref_service.get_all()
+    refs = ref_service.get_all_meta()
     return render_template(
         "index.html",
         nav="index",
@@ -15,15 +15,14 @@ def index():
     )
 
 
-@app.get("/new_reference")
+@app.get("/new-reference")
 def show_new_reference():
-    ref_type = request.args.get("type")
-    ref_type = ref_type if ref_type else "book"
+    ref_type = request.args.get("type") or "book"
     required, optional = get_reference_fields(ref_type)
     all_refs = sorted(list(get_reference_types()))
 
     return render_template(
-        "new_reference.html",
+        "new-reference.html",
         nav="new",
         all_refs=all_refs,
         ref_type=ref_type,
@@ -32,17 +31,19 @@ def show_new_reference():
     )
 
 
-@app.post("/new_reference")
+@app.post("/new-reference")
 def new_reference():
-    ref_type = request.form.get("type")
-    ref_name = request.form.get("name")
+    ref_type = request.form.get("ref-type-input")
+    ref_key = request.form.get("ref-key-input")
     fields = {
-        key: value for key, value in request.form.items() if key not in ("type", "name")
+        key: value
+        for key, value in request.form.items()
+        if key not in ("ref-type-input", "ref-key-input", "field-select") and value
     }
 
     ref_data = {
         "type": ref_type,
-        "name": ref_name,
+        "name": ref_key,
         "fields": fields,
     }
 
@@ -56,7 +57,7 @@ def new_reference():
         return redirect(url_for("show_new_reference", type=ref_type))
 
 
-@app.get("/edit_reference")
+@app.get("/edit-reference")
 def show_edit_reference():
     ref_id = request.args.get("id")
     ref = ref_service.get(ref_id=ref_id)
@@ -67,7 +68,7 @@ def show_edit_reference():
     ref.optional = set(field.type for field in ref.fields if field.type in optional)
 
     return render_template(
-        "edit_reference.html",
+        "edit-reference.html",
         nav="index",
         ref=ref,
         ref_types=ref_types,
@@ -76,20 +77,21 @@ def show_edit_reference():
     )
 
 
-@app.post("/edit_reference")
+@app.post("/edit-reference")
 def edit_reference():
-    ref_id = request.form.get("id")
-    ref_type = request.form.get("type")
-    ref_name = request.form.get("name")
+    ref_id = request.args.get("id")
+    ref_type = request.form.get("ref-type-input")
+    ref_key = request.form.get("ref-key-input")
     fields = {
         key: value
         for key, value in request.form.items()
-        if key not in ("id", "type", "name")
+        if key not in ("id", "ref-type-input", "ref-key-input", "field-select")
+        and value
     }
 
     ref_data = {
         "type": ref_type,
-        "name": ref_name,
+        "name": ref_key,
         "fields": fields,
     }
 
@@ -100,4 +102,4 @@ def edit_reference():
 
     except ValueError as error:
         flash(str(error), "error")
-        return redirect(url_for("edit_reference", ref_id=ref_id, type=ref_type))
+        return redirect(url_for("edit_reference", ref_id=ref_id))
--- src/services/reference_service.py
+++ src/services/reference_service.py
@@ -1,21 +1,35 @@
-from typing import Iterable
-
-from sqlalchemy import ScalarResult
-
 from src.models.reference import Reference
 from src.repositories.reference_repository import reference_repository
 from src.services.input_validation import validate_reference
+from src.services.reference_types import get_reference_fields
 
 
 class ReferenceService:
     def __init__(self, repo):
         self._repo = repo
 
-    def get_all(self) -> Iterable[ScalarResult]:
+    def get_all(self) -> list:
         return self._repo.get_all()
+
+    def get_all_meta(self) -> list:  # injects useful data about fields
+        refs = self._repo.get_all()
+        for ref in refs:
+            ref.required, ref.optional = get_reference_fields(ref.type)
+            ref.required_count = len([f for f in ref.fields if f.type in ref.required])
+            ref.optional_count = len([f for f in ref.fields if f.type in ref.optional])
+
+        return refs
 
     def get(self, ref_id: id = None) -> Reference:
         return self._repo.get(ref_id)
+
+    def get_meta(self, ref_id: id = None) -> Reference:
+        ref = self._repo.get(ref_id)
+        ref.required, ref.optional = get_reference_fields(ref.type)
+        ref.required_count = len([f for f in ref.fields if f.type in ref.required])
+        ref.optional_count = len([f for f in ref.fields if f.type in ref.optional])
+
+        return ref
 
     def create(self, ref_data: dict) -> Reference:
         validate_reference(ref_data)
