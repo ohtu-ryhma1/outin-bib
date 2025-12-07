bcea9f063683a3ed3a2c28a1997c0a1ee2123d8c
hatonlee <hatonen.leevi@gmail.com>
2025-12-07 20:08:29 +0200

    feat: updated showing and editing reference
    Unified css and js to one file

--- src/static/scripts/show-reference.js
+++ src/static/scripts/show-reference.js
@@ -1,5 +1,6 @@
 // event listener for changing reference type
 const refTypeSelect = document.querySelector("#ref-type-input");
+
 refTypeSelect.addEventListener("change", () => {
     const refTypes = refTypeSelect.children;
     const selectedOption = refTypes[refTypeSelect.selectedIndex].value;
@@ -9,12 +10,15 @@ refTypeSelect.addEventListener("change", () => {
 );
 
 // event listener for adding fields
-const addBtn = document.querySelector("#add-field");
+const addBtn = document.querySelector("#add-field-button");
+const fieldSelect = document.querySelector("#field-select");
+const fields = document.querySelector("#fields");
+const fieldAdder = document.querySelector("#field-adder")
+
 addBtn.addEventListener("click", () => {
-  // get selected field type
-  const optionalSelect = document.querySelector("#field-select");
-  const field = optionalSelect.children[optionalSelect.selectedIndex];
-  const fieldType = field.value;
+  // get selected field type and value
+  const selectedField = fieldSelect.children[fieldSelect.selectedIndex];
+  const fieldType = selectedField.value;
 
   // create label for new field type
   const label = document.createElement("label");
@@ -25,20 +29,19 @@ addBtn.addEventListener("click", () => {
   const textarea = document.createElement("textarea");
   textarea.name = fieldType;
   textarea.id = fieldType;
-  textarea.classList = ["field-value"]
+  textarea.classList = ["field-value input-select"]
   textarea.rows = 1;
   textarea.maxLength = 500;
 
-  // append label and textarea to document
-  const optionalFieldsContainer = document.querySelector("#fields");
-  optionalFieldsContainer.appendChild(label);
-  optionalFieldsContainer.appendChild(textarea);
+  // append label and textarea to fields
+  fields.appendChild(label);
+  fields.appendChild(textarea);
 
   // delete selected optional field type
-  optionalSelect.removeChild(field);
+  fieldSelect.removeChild(selectedField);
 
   // remove the select-element if no more optional fields can be added
-  if (optionalSelect.children.length === 0) {
-    document.querySelector("#field-adder").remove();
+  if (fieldSelect.children.length === 0) {
+    fieldAdder.remove();
   }
 });
