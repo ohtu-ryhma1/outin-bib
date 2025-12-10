88c6e0b5bd27ad3fd15a85d11dd2799917d68590
hatonlee <hatonen.leevi@gmail.com>
2025-12-10 10:10:20 +0200

    feat: renamed method for creating references

--- src/robot/libraries/app_library.py
+++ src/robot/libraries/app_library.py
@@ -8,8 +8,8 @@ class AppLibrary:
     def reset_db(self):
         requests.post(f"{self._base_url}/test/reset-db", timeout=5)
 
-    def create_reference_directly(self, ref_type, ref_key, fields):
-        ref_data = {"ref-type-input": ref_type, "ref-key-input": ref_key, **fields}
+    def create_reference_via_request(self, ref_type, ref_key, fields):
+        ref_data = {"ref-type-select": ref_type, "ref-key-input": ref_key, **fields}
 
         requests.post(f"{self._base_url}/new-reference", data=ref_data, timeout=5)
 
