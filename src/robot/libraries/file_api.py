e26fd5377000500e1835b33d01d135c8f082e8a1
MarkusSKoo <markus.kauhanen@helsinki.fi>
2025-12-11 18:44:05 +0200

    fix(tests): ignore .crdownload files when checking latest export

--- src/robot/libraries/file_api.py
+++ src/robot/libraries/file_api.py
@@ -2,8 +2,12 @@ import os
 
 
 class FileApi:
-    def get_latest_file(self, directory: str) -> str:
-        files = [os.path.join(directory, f) for f in os.listdir(directory)]
+    def get_latest_exported_file_path(self, directory: str) -> str:
+        files = [
+            os.path.join(directory, f)
+            for f in os.listdir(directory)
+            if f.endswith(".bib")
+        ]
         latest_file = max(files, key=os.path.getctime)
         return latest_file
 
